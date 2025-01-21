374da7be7c6f61241c7dab4fb102b19228df0de5
Chandudixitt <yelheriv@gmail.com>
2025-01-21 17:25:58 +0530

    newcommit

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket = "evertz-demo-terraform-bucket"
-    key    = "terraform.tfstate"
+    key    = "terraform_state_file/terraform.tfstate"
     region = "us-west-1"
   }
 }
